fb4941a600534268657ee59b03552d803578f45f
Crazy_Fox <holmyliszag@ukr.net>
2025-11-11 18:52:58 +0200

    finish HW

---
+++ src/task-7.ts
@@ -0,0 +1,13 @@
+function getMessage(): Promise<string> {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve('Hello from TS');
+    }, 1000);
+  });
+}
+
+getMessage().then(result => console.log(result));
+
+setTimeout(() => {
+  console.log(' ');
+}, 1000);
